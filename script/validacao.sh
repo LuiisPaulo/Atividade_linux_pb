e64c8f9416b08ac191d5de558126a0b5e8ebccf8
root <root@ip-10-0-10-10.ec2.internal>
2023-07-12 19:58:32 +0000

    first commit

---
+++ script/validacao.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if systemctl is-active httpd>/dev/null 2>&1;
+then
+status="Online"
+mensagem="Servidor está $status em $(date '+%d-%m-%Y %H:%M:%S')"
+echo "$mensagem" > /home/ec2-user/atividade/efs/status_apache/status_online.txt
+else
+status="Offline"
+mensagem="Servidor Apache está $status em $(date '+%d-%m-%Y %H:%M:%S')"
+echo "$mensagem" >  /home/ec2-user/atividade/efs/status_apache/status_offline.txt
+fi
