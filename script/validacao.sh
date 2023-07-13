4f00c95494da362031cec13786b5fa476177a7a4
LuiisPaulo <138496370+LuiisPaulo@users.noreply.github.com>
2023-07-13 13:38:30 -0300

    Update validacao.sh

--- script/validacao.sh
+++ script/validacao.sh
@@ -4,9 +4,9 @@ if systemctl is-active httpd>/dev/null 2>&1;
 then
 status="Online"
 mensagem="Servidor está $status em $(date '+%d-%m-%Y %H:%M:%S')"
-echo "$mensagem" > /home/ec2-user/atividade/efs/status_apache/status_online.txt
+echo "$mensagem" >> /home/ec2-user/atividade/efs/status_apache/status_log.txt
 else
 status="Offline"
 mensagem="Servidor Apache está $status em $(date '+%d-%m-%Y %H:%M:%S')"
-echo "$mensagem" >  /home/ec2-user/atividade/efs/status_apache/status_offline.txt
+echo "$mensagem" >> /home/ec2-user/atividade/efs/status_apache/status_log.txt
 fi
