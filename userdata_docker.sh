2e7c9b5c86049262f08e1e4d2e92ec8dd4c7464a
Luis Paulo <luispaulolopesg@gmail.com>
2023-08-21 11:58:22 -0300

    refatoração códigos

--- userdata_docker.sh
+++ userdata_docker.sh
@@ -4,16 +4,23 @@ sudo yum install git -y
 sudo yum install yum-utils -y
 sudo yum install nfs-utils -y
 
+# Configurando o EFS
+sudo yum update -y
+sudo yum install -y amazon-efs-utils
+sudo mkdir /efs
+#colar o ponto de montagem que o serviço de EFS fornece
+
 # Instalando docker
 sudo yum update -y
 sudo yum install docker -y
-sudo usermod -aG docker ec2-user
+sudo yum update -y
 sudo chmod 666 /var/run/docker.sock
-sudo newgrp docker
+
 
 # Instalando docker-compose
-curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo yum update -y
+sudo curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 # Ativando o serviço docker
 sudo systemctl enable docker.service
